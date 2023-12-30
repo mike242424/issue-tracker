f86a033ae367c5eb67ad3ad5077827b08724549f
mike242424 <mikebyers24@gmail.com>
2023-12-30 11:11:08 -0500

    build the new issue page

--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -1,7 +1,12 @@
 import { Button } from '@radix-ui/themes';
+import Link from 'next/link';
 
 const IssuesPage = () => {
-  return <Button>Add Issue</Button>;
+  return (
+    <Button>
+      <Link href="/issues/new">Add New Issue</Link>
+    </Button>
+  );
 };
 
 export default IssuesPage;
